5d2c052147da6d96135a93c4c621a4f858181806
OTKMOB <superlowkey@outlook.com>
2023-02-04 02:54:41 -0800

    feat: get user follow info

--- biz/dal/model/user.go
+++ biz/dal/model/user.go
@@ -2,6 +2,7 @@ package model
 
 import (
 	"BiteDans.com/tiktok-backend/biz/dal"
+	"BiteDans.com/tiktok-backend/biz/model/douyin/core/user"
 	"gorm.io/gorm"
 )
 
@@ -27,6 +28,45 @@ func FindUserByUsername(u *User, username string) error {
 
 func CreateUser(u *User) error {
 	return dal.DB.Create(u).Error
+}
+
+func GetFollowInfoByIDs(from_user_id uint, to_user_id uint, user_resp *user.User) error {
+	from_user := User{}
+	to_user := User{}
+
+	var err error
+
+	err = FindUserById(&from_user, from_user_id)
+	if err != nil {
+		return err
+	}
+
+	err = FindUserById(&to_user, to_user_id)
+	if err != nil {
+		return err
+	}
+
+	result := []User{}
+	err = dal.DB.Model(&from_user).Where("follow_id", to_user_id).Association("Followings").Find(&result)
+	if err != nil {
+		return err
+	}
+
+	user_resp.ID = int64(to_user.ID)
+	user_resp.Name = to_user.Username
+	user_resp.FollowCount = getFollowCount(&to_user)
+	user_resp.FollowerCount = getFollowerCount(&to_user)
+	user_resp.IsFollow = !(len(result) == 0)
+
+	return nil
+}
+
+func getFollowCount(user *User) int64 {
+	return dal.DB.Model(user).Association("Followings").Count()
+}
+
+func getFollowerCount(user *User) int64 {
+	return dal.DB.Model(user).Association("Followers").Count()
 }
 
 func UserFollowAction(uc *User, ut *User, t uint) error {
