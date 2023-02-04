ad9d14dc730fb930678a63803a98f2c9a541141e
James Zhang <zyz991016@outlook.com>
2023-02-03 23:07:31 -0800

    feat: update chat.thrift

--- biz/router/register.go
+++ biz/router/register.go
@@ -6,6 +6,7 @@ import (
 	douyin_core_user "BiteDans.com/tiktok-backend/biz/router/douyin/core/user"
 	douyin_core_video "BiteDans.com/tiktok-backend/biz/router/douyin/core/video"
 	douyin_extra_follow "BiteDans.com/tiktok-backend/biz/router/douyin/extra/follow"
+	douyin_extra_message "BiteDans.com/tiktok-backend/biz/router/douyin/extra/message"
 	hello_example "BiteDans.com/tiktok-backend/biz/router/hello/example"
 	"github.com/cloudwego/hertz/pkg/app/server"
 )
@@ -15,6 +16,8 @@ func GeneratedRegister(r *server.Hertz) {
 	//INSERT_POINT: DO NOT DELETE THIS LINE!
 	douyin_core_video.Register(r)
 	douyin_extra_follow.Register(r)
+	douyin_extra_message.Register(r)
+
 	douyin_core_user.Register(r)
 
 	hello_example.Register(r)
